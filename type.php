9cc49c3a65fee533af609731d96715ed1a2136b4
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2022-08-29 17:17:46 -0300

    if and switch

--- type.php
+++ type.php
@@ -1,9 +1,28 @@
 <?php
   $a_int = 1;
-  $b_bool = TRUE ;
+  $b_bool = FALSE ;
   $c_str = 'rodrigo';
   $d_str = "Juliete";
+  
+  // if ($b_bool != FALSE){
+  //   echo 'Essa variável é verdade';
+  // } else{
+  //   echo'Essa variável é falsa';
+  // }
+    $cerveja = 'amstel';
+  switch($cerveja)
+  {
+      case 'amstel';
+      case 'stella artois';
+      case 'heineken';
+          echo 'Boa escolha';
+          break;
+      default;
+          echo 'Por favor faça uma nova seleção...';
+          break;
+  }
 
-  echo  gettype($d_str);
+  // echo 'Essa variável é do tipo:' .' '. gettype($d_str);
+
 
 ?>
